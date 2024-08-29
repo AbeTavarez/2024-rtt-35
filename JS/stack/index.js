ab33e281a92620511098fbb754801fcafa9a719d
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-08-29 10:59:19 -0400

    import export lab

--- JS/stack/index.js
+++ JS/stack/index.js
@@ -1,14 +1,3 @@
-// counter variable
-let counter = 0;
 
-function increment() {
-  try {
-    counter += 1;
-    increment();
-  } catch (e) {
-    console.log(counter);
-    console.error(e);
-  }
-}
 
-// increment();
+const stack = new Stack()
