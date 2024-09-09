df050167fe8c764f988fde1ad867c7042d5a0457
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-09-09 16:36:57 -0400

    react fashio blog

---
+++ react/alab320h-react/src/components/Article1.jsx
@@ -0,0 +1,19 @@
+function Article1() {
+  return (
+    <article>
+      <h2>Article 1</h2>
+      <div>
+        <img src="/blog-image-1.jpg" alt="" />
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis cumque
+        saepe reprehenderit veritatis corrupti fugiat, ex nisi debitis nulla
+        natus beatae enim officiis expedita, illo assumenda nam exercitationem
+        illum impedit.
+      </p>
+      <button>continues...</button>
+    </article>
+  );
+}
+
+export default Article1;
