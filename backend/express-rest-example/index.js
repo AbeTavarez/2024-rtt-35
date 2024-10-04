b61847b37a2c3d2d084a4cafa9abf852ed943c9a
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-10-04 11:20:09 -0400

    express api server

--- backend/express-rest-example/index.js
+++ backend/express-rest-example/index.js
@@ -6,40 +6,81 @@ import { posts } from "./data/posts.js";
 const app = express();
 const PORT = 4000;
 
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+
 // ======= API Routes
+
+/**
+ * GET
+ */
 app.get("/api/users", (req, res) => {
   res.json(users);
 });
 
-app.get("/api/users/:id", (req, res) => {
+/**
+ * GET by id
+ */
+app.get("/api/users/:id", (req, res, next) => {
   console.log(req.params);
   const user = users.find((user) => user.id == req.params.id);
   if (user) {
     res.json(user);
   } else {
-    res.json({ error: "User not found" });
+    next(); // calls the custom 404 middleware
   }
 });
 
-app.get('/api/posts', (req, res) => {
-    res.json(posts)
+/**
+ * POST
+ */
+app.post("/api/users", (req, res) => {
+  console.log(req.body);
+
+  if (req.body.name && req.body.username && req.body.email) {
+    if (users.find((u) => u.username == req.body.username)) {
+      res.json({ error: "Username Already Taken" });
+      return;
+    }
+
+    const user = {
+      id: users[users.length - 1].id + 1,
+      name: req.body.name,
+      username: req.body.username,
+      email: req.body.email,
+    };
+
+    users.push(user);
+    res.json(users[users.length - 1]);
+  }
 });
 
-app.get('/api/posts/:id', (req, res) => {
-    console.log(req.params);
-    const post = posts.find(post => post.id == req.params.id)
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
 
-    if (post) {
-        res.json(post)
-    } else {
-        res.json({error: `Post with id ${req.params.id} was not found!`})
-    }
-    
-})
+app.get("/api/posts/:id", (req, res, next) => {
+  console.log(req.params);
+  const post = posts.find((post) => post.id == req.params.id);
+
+  if (post) {
+    res.json(post);
+  } else {
+    next(); // calls the custom 404 middleware
+  }
+});
 
 // ======== Routes
 app.get("/", (req, res) => {
   res.send("ok");
+});
+
+// ======= Error middlewares
+
+// Custom 404 (not found) middleware.
+app.use((req, res) => {
+  res.status(404).json({ error: "Resource Not Found" });
 });
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
